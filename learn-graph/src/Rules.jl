10cc96c6a0ebb468950b876691fdf41b0e01d170
gilgameshjw <gilgameshjw@protonmail.com>
2022-05-06 00:20:32 +0100

    finalised code from diagram, just need to clean up

--- learn-graph/src/Rules.jl
+++ learn-graph/src/Rules.jl
@@ -60,20 +60,117 @@ dicCODE["bind transliterated words together"] =
          d),
         Dict(:in => ["l_transl_wrds"], :out => ["txt"]))
 
-        dicCODE["apply mappings on each word"] =
-            Functor((d,e=nothing,f=nothing) ->
+dicCODE["process each word with mapping"] =
+    Functor((d,e=nothing,f=nothing) ->
+        begin
+            d["l_transl_wrds"] =
+            map(wrd ->
                 begin
-                  d["l_transl_wrds"] =
-                    map(wrd ->
-                        begin
-                            dd = copy(dataSTATE)
-                            dd["wrd"] = wrd
-                            interfaceName = "mapping"
-                            node = e[interfaceName]
-                            runAgent(node, e, f, dd)
-                        end,
-                        d["l_wrds"])
-                  d
+                    dd = copy(dataSTATE)
+                    dd["wrd"] = wrd
+                    interfaceName = "mapping"
+                    node = e[interfaceName]
+                    runAgent(node, e, f, dd)
                 end,
-                Dict(:in => ["l_wrds"],
-                     :out => ["l_transl_wrds"]))
+                d["l_wrds"])
+                d
+        end,
+            Dict(:in => ["l_wrds"],
+                 :out => ["l_transl_wrds"]))
+
+dicCODE["has word the char z?"] =
+    Functor((d,e=nothing,f=nothing) ->
+        (d["state"] = occursin("z", d["wrd"]) ?
+                    "yes" : "no";
+         d),
+         Dict(:in => ["wrd"], :out => ["wrd", "state"]))
+
+
+dicCODE["map z to @"] =
+    Functor((d,e=nothing,f=nothing) ->
+        (wrd = d["wrd"];
+         d["wrd"] =
+            read(pipeline(`echo $wrd`, `sed s/z/@/g`), String);
+         d),
+         Dict(:in => ["wrd"], :out => ["wrd"]))
+
+
+using PyCall
+
+py"""
+d_maps = {'z': 'a',
+          'y': 'x',
+          'x': 'w',
+          'w': 'v',
+          'v': 'u',
+          'u': 't',
+          't': 's',
+          's': 'r',
+          'r': 'q',
+          'q': 'p',
+          'p': 'o',
+          'o': 'n',
+          'n': 'm',
+          'm': 'l',
+          'l': 'k',
+          'k': 'j',
+          'j': 'i',
+          'i': 'h',
+          'h': 'g',
+          'g': 'f',
+          'f': 'e',
+          'e': 'd',
+          'd': 'e',
+          'c': 'd',
+          'b': 'c',
+          'a': 'b',
+          '@': 'a'}
+
+"""
+
+dicCODE["map all letters utilising table and @ to a"] =
+    #===
+        We use a python dictionary to generate the mappings.
+        Alternative snippet structure with begin; end;
+    ===#
+    Functor((d,e=nothing,f=nothing) ->
+        begin
+            wrd = d["wrd"]
+            d["wrd"] = join([py"""d_maps"""[string(c)] for c in wrd
+                     if c!='\n'])
+                         d
+        end,
+            Dict(:in => ["wrd"], :out => ["wrd"]))
+
+py"""
+latin_chars = 'abcdefghijklmnopqrstuvwxyz '
+
+def normalise_filter_txt(txt):
+    txt = txt.lower().strip()
+    txt = ' '.join(txt.split())
+    return ''.join([c for c in txt if c in latin_chars])
+
+"""
+
+dicCODE["normalize the text!"] =
+    #===
+        Example calling a python function
+    ===#
+    Functor((d,e=nothing,f=nothing) ->
+        (println(d);
+         d["txt"] = py"""normalise_filter_txt"""(d["txt"]);
+         d),
+            Dict(:in => ["txt"], :out => ["txt"]))
+
+
+dicCODE["tokenize the text!"] =
+#===
+    Example running julia code
+        Function
+            :in => txt
+            :out => l_wrds
+===#
+Functor((d,e=nothing,f=nothing) ->
+    (d["l_wrds"] = split(d["txt"]);
+     d),
+        Dict(:in => ["txt"], :out => ["l_wrds"]))

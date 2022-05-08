d6a22c7c81dbd184b81d0c1ec9d80ad1294ef807
gilgameshjw <gilgameshjw@protonmail.com>
2022-05-08 17:36:45 +0100

    written ISO rules

--- learn-graph/src/Rules.jl
+++ learn-graph/src/Rules.jl
@@ -1,29 +1,14 @@
 
-#===
+# https://lucid.app/lucidchart/008ff36b-136f-4666-b06e-ac3b2a8e8d85/edit?page=0_0&invitationId=inv_0221b3ba-bd16-4499-8f60-b95aa68af45c
 
-    Python Code:
-
-    Can be also put in another file. or alternatively Below:
-
-using PyCall # julia package to interact with python
-
-py"""
-
-latin_chars = 'abcdefghijklmnopqrstuvwxyz '
-
-def normalise_filter_txt(txt):
-    txt = txt.lower().strip()
-    txt = ' '.join(txt.split())
-    return ''.join([c for c in txt if c in latin_chars])
-
-...
-
-"""
-
-===#
+# Read Mappings
+dISOMapping = map(x -> split(x), #|> println,
+    readlines("resources/Strict+ShortVowelsOnlyIf.txt")) |>
+    (D -> filter(x -> length(x) > 1, D)) |>
+    (D -> map(d -> d[1] => d[2], D)) |> Dict ;
 
 
-# computations state
+# Computations state
 dataSTATE = Dict{String, Any}(
             "state" => nothing,
             "brain" => nothing);
@@ -34,147 +19,42 @@ dicCODE = Dict{String, Functor}()
 
 
 # Node terminating computation
-dicCODE["done, terminate"] =
-    #===
-        Basic form of functors:
-            d: data
-            e: dicBRAINS
-            f: df_Nodes
 
-        Inputs and Outputs are specified
-        :in => "l_transliterated" # list of
-        :out => "res" # field expected at end of (sub)sequence
+dicCODE["return the character!"] =
+    Functor((d,e=nothing,f=nothing) ->
+        (d["res"] = d["txt"]; d),
+        Dict(:in => ["txt"], :out => ["res"]))
+
+dicCODE["return its transliteration!"] =
+    Functor((d,e=nothing,f=nothing) ->
+        (d["res"] = d["txt"]; d),
+        Dict(:in => ["txt"], :out => ["res"]))
+
+dicCODE["is the character found in the mapping?"] =
+    Functor((d,e=nothing,f=nothing) ->
+        (d["state"] = haskey(dISOMapping, d["char"]) ? "yes" : "no"; d),
+        Dict(:in => ["char"], :out => ["state"]))
+
+dicCODE["change all instances of ي and ك and ۀ in the text to ی and ک and هٔ"] =
+    #===
+    d_corrects = {'ي' : 'ی',
+                  'ك' : 'ک',
+                 'ۀ' : 'هٔ'}
     ===#
     Functor((d,e=nothing,f=nothing) ->
         begin
-            d["res"] = d["transl_txt"]
-            # clean up if necessary
-            delete!(d, "transl_txt")
-            # or
-            [delete!(d, f) for f in ["l_transl_wrds", "transl_txt", "l_wrds"]]
-            # return dictionary
+            d["txt"] = replace(d["txt"], 'ك'=>
+                                         'ک',
+                                            'ي'=>
+                                            'ی',
+                                                "ۀ" =>
+                                                "هٔ")
             d
-        end, # identity
-        Dict(:in => ["transl_txt"], :out => ["res"]))
-
-
-dicCODE["bind transliterated words together"] =
-    #===    ===#
-    Functor((d,e=nothing,f=nothing) ->
-        (d["transl_txt"] = join(d["l_transl_wrds"], " ");
-         d),
-        Dict(:in => ["l_transl_wrds"], :out => ["transl_txt"]))
-
-dicCODE["process each word with mapping"] =
-    Functor((d,e=nothing,f=nothing) ->
-        begin
-            d["l_transl_wrds"] =
-            map(wrd ->
-                begin
-                    dd = copy(dataSTATE)
-                    dd["wrd"] = wrd
-                    interfaceName = "mapping"
-                    node = e[interfaceName]
-                    runAgent(node, e, f, dd)
-                end,
-                d["l_wrds"])
-                d
         end,
-            Dict(:in => ["l_wrds"],
-                 :out => ["l_transl_wrds"]))
+        Dict(:in => ["txt"], :out => ["txt"]))
 
-dicCODE["has word the char z?"] =
+dicCODE["is it ّ?"] =
     Functor((d,e=nothing,f=nothing) ->
-        (d["state"] = occursin("z", d["wrd"]) ?
-                    "yes" : "no";
-         d),
-         Dict(:in => ["wrd"], :out => ["wrd", "state"]))
-
-
-dicCODE["map z to @"] =
-    Functor((d,e=nothing,f=nothing) ->
-        (wrd = d["wrd"];
-         d["wrd"] =
-            read(pipeline(`echo $wrd`, `sed s/z/@/g`), String);
-         d),
-         Dict(:in => ["wrd"], :out => ["wrd"]))
-
-
-using PyCall
-
-py"""
-d_maps = {'z': 'a',
-          'y': 'z',
-          'x': 'y',
-          'w': 'x',
-          'v': 'w',
-          'u': 'v',
-          't': 'u',
-          's': 't',
-          'r': 's',
-          'q': 'r',
-          'p': 'q',
-          'o': 'p',
-          'n': 'o',
-          'm': 'n',
-          'l': 'm',
-          'k': 'l',
-          'j': 'k',
-          'i': 'j',
-          'h': 'i',
-          'g': 'h',
-          'f': 'g',
-          'e': 'f',
-          'd': 'e',
-          'c': 'd',
-          'b': 'c',
-          'a': 'b',
-          '@': 'a'}
-
-"""
-
-dicCODE["map all letters utilising table and @ to a"] =
-    #===
-        We use a python dictionary to generate the mappings.
-        Alternative snippet structure with begin; end;
-    ===#
-    Functor((d,e=nothing,f=nothing) ->
-        begin
-            wrd = d["wrd"]
-            d["res"] = join([py"""d_maps"""[string(c)] for c in wrd
-                     if c!='\n'])
-                         d
-        end,
-            Dict(:in => ["wrd"], :out => ["res"]))
-
-py"""
-latin_chars = 'abcdefghijklmnopqrstuvwxyz '
-
-def normalise_filter_txt(txt):
-    txt = txt.lower().strip()
-    txt = ' '.join(txt.split())
-    return ''.join([c for c in txt if c in latin_chars])
-
-"""
-
-dicCODE["normalize the text!"] =
-    #===
-        Example calling a python function
-    ===#
-    Functor((d,e=nothing,f=nothing) ->
-        (d["txt"] = py"""normalise_filter_txt"""(d["txt"]);
-         d),
-            Dict(:in => ["txt"], :out => ["txt"]))
-
-
-dicCODE["tokenize the text!"] =
-#===
-    Example running julia code
-        Function
-            :in => txt
-            :out => l_wrds
-===#
-Functor((d,e=nothing,f=nothing) ->
-    (d["l_wrds"] = split(d["txt"]);
-     d),
-        Dict(:in => ["txt"], :out => ["l_wrds"]))
+        (d["state"] = d["char"] == ''' ?
+            "yes" : "no"; d),
+        Dict(:in => ["char"], :out => ["state"]))
